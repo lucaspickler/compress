1c0e0c4f3fb2bd258567060ea95b8d3ef4760ef7
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-09-05 12:31:13 -0300

    updating copyright

--- include/global.h
+++ include/global.h
@@ -1,7 +1,20 @@
 /*
- * Copyright(C) 2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * Copyright(C) 2014-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
- * <global.h> - Global variables and functions.
+ * This file is part of compress.
+ * 
+ * compress is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * compress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with compress. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GLOBAL_H_
@@ -9,9 +22,7 @@
 	
 	#include <stdio.h>
 	
-	/*
-	 * Compress/decompress a file using LZW algorithm.
-	 */
-	extern void lzw(FILE *input, FILE *output, int compress);
+	/* Forward definitions */
+	extern void lzw(FILE *, FILE *, int);
 
 #endif /* GLOBAL_H_ */
--- src/util.c
+++ src/util.c
@@ -1,20 +1,20 @@
 /*
  * Copyright(C) 2014-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
- * This file is part of LZW.
+ * This file is part of compress.
  * 
- * LZW is free software; you can redistribute it and/or modify
+ * compress is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
- * LZW is distributed in the hope that it will be useful,
+ * compress is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with LZW. If not, see <http://www.gnu.org/licenses/>.
+ * along with compress. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
